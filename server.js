d52cf5c3cc65be92fc6643a7ba6553e42073acb4
Leo Mucci <leocapramucci@gmail.com>
2023-11-24 14:51:47 -0300

    asdfaf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Sequelize } = require('sequelize');
 const config = require('./config');
-const Anime = require('./Models/Anime');
+
 
 const app = express();
 const port = 80;
