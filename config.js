585ad88e78ef98ef31c515d9a18230f0785eb7ee
Leo Mucci <leocapramucci@gmail.com>
2023-11-24 15:30:19 -0300

    sds

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
         database: 'db-jrtmmxyz40e7',
         username: 'db-jrtmmxyz40e7',
-        password: 'P2ZSG7e7iArghan3oLAS3cDd3',
+        password: '2ZSG7e7iArghan3oLAS3cDd3',
         host: 'up-es-mad1-mysql-1.db.run-on-erla.com',
         port: '11550',
         dialect: 'mysql'
